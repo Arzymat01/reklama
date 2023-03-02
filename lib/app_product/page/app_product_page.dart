592a927a61e06bb3336d1da310806c5ebdd8dad2
Arzymat <104361795+Arzymat01@users.noreply.github.com>
2023-03-02 20:48:05 +0600

    photo ad galeri

--- lib/app_product/page/app_product_page.dart
+++ lib/app_product/page/app_product_page.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
+import 'package:jarnama/components/image_container.dart';
 
 import '../../components/cusstomTextfield.dart';
 import '../../services/date_time.dart';
@@ -43,6 +44,7 @@ class AppProductPage extends StatelessWidget {
           ImageContainer(
             images: images,
             onPicked: (value) => images = value,
+            onDelete: (xfile) => images.remove(xfile),
           ),
           const SizedBox(height: 12),
           CustumTextField(
@@ -89,79 +91,3 @@ class AppProductPage extends StatelessWidget {
 }
 
 // ignore: must_be_immutable
-class ImageContainer extends StatefulWidget {
-  ImageContainer({
-    super.key,
-    required this.images,
-    required this.onPicked,
-  });
-
-  List<XFile> images;
-  final void Function(List<XFile> image) onPicked;
-
-  @override
-  State<ImageContainer> createState() => _ImageContainerState();
-}
-
-class _ImageContainerState extends State<ImageContainer> {
-  final services = ImagePicerServices();
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        width: double.infinity,
-        height: 200,
-        decoration: BoxDecoration(
-          border: Border.all(),
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: widget.images.isNotEmpty
-            ? SizedBox(
-                child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                  ),
-                  itemCount: widget.images.length,
-                  itemBuilder: (BuildContext context, index) {
-                    return ImageCard(
-                      file: widget.images[index],
-                    );
-                  },
-                ),
-              )
-            : Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(50),
-                  child: IconButton(
-                    onPressed: () async {
-                      final value = await services.pickImages();
-                      if (value != null) {
-                        widget.onPicked(value);
-                        widget.images = value;
-                        setState(() {});
-                      }
-                    },
-                    icon: const Icon(
-                      Icons.camera_enhance,
-                      size: 30,
-                    ),
-                  ),
-                ),
-              ));
-  }
-}
-
-class ImageCard extends StatelessWidget {
-  const ImageCard({
-    super.key,
-    required this.file,
-  });
-
-  final XFile file;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      child: Image.file(File(file.path)),
-    );
-  }
-}
